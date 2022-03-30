a78bc72800f698a95b3a77807efcfbe11bc5b254
Dhaval1933 <78550108+Dhaval1933@users.noreply.github.com>
2022-03-30 11:58:35 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+        apiKey: "AIzaSyBCl8IsbX5mS3snDjMWahzrhW7Ds1hUrho",
+        authDomain: "proj-83bc9.firebaseapp.com",
+        projectId: "proj-83bc9",
+        storageBucket: "proj-83bc9.appspot.com",
+        messagingSenderId: "19185750407",
+        appId: "1:19185750407:web:7fb9303bee04514bf40322"     
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
